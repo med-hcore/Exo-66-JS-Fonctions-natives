54b8309c4ba3325bd6c42a0e1cfed504fe52396d
med-hcore <mehdi.ca2018@gmail.com>
2020-12-04 10:00:28 +0100

    fonction natives

---
+++ addFonctionNatives.js
@@ -0,0 +1,12 @@
+let entier = document.getElementById("entier");
+let flottant = document.getElementById("flottant");
+let nonNum = document.getElementById("non-numerique");
+let rand = document.getElementById("random");
+let trunc = document.getElementById("trunc");
+let repl = document.getElementById("replace");
+
+let operande = 10;
+
+entier.innerHTML = (32 * operande).toString()
+
+flottant.innerHTML = (54.85 * operande) + entier.toString()
